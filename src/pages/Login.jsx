ad81030b99f1859b0027e45ccdcfcc33862224f5
JesusMendoza815 <jesuscamposmendoza815@gmail.com>
2023-05-17 21:16:22 -0600

    hotfix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useContext } from "react";
-import Navbar from "../components/Navbar";
+import { useState, useContext } from "react";
 import "../styles/Login.scss";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../Context/AuthContext";
@@ -58,40 +57,39 @@ function Login() {
       {!isLoading && error && <h2>Hubo un erro al mostrar los personajes</h2>}
       {!isLoading && !error && (
         <>
-          <Navbar />
           <h1 className="flex justify-center titleLogin">
             Welcome please Login
           </h1>
-          <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
               <img
-                class="mx-auto h-30 w-auto"
+                className="mx-auto h-30 w-auto"
                 src="https://media.tenor.com/Tg8iZVA-pecAAAAM/nickimperiod.gif"
                 alt="Your Company"
               />
-              <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                 Sign in to your account
               </h2>
             </div>
 
-            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-              <form class="space-y-6" action="#" method="POST">
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form className="space-y-6" action="#" method="POST">
                 <div>
                   <label
-                    for="email"
-                    class="block text-sm font-medium leading-6 text-gray-900"
+                    htmlFor="email"
+                    className="block text-sm font-medium leading-6 text-gray-900"
                   >
                     Email address
                   </label>
-                  <div class="mt-2">
+                  <div className="mt-2">
                     <input
                       id="email"
                       name="email"
                       type="email"
                       value={user}
-                      autocomplete="email"
+                      autoComplete="email"
                       required
-                      class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       onChange={({ target }) => {
                         setUser(target.value);
                       }}
@@ -100,31 +98,31 @@ function Login() {
                 </div>
 
                 <div>
-                  <div class="flex items-center justify-between">
+                  <div className="flex items-center justify-between">
                     <label
-                      for="password"
-                      class="block text-sm font-medium leading-6 text-gray-900"
+                      htmlFor="password"
+                      className="block text-sm font-medium leading-6 text-gray-900"
                     >
                       Password
                     </label>
-                    <div class="text-sm">
+                    <div className="text-sm">
                       <a
                         href="#"
-                        class="font-semibold text-indigo-600 hover:text-indigo-500"
+                        className="font-semibold text-indigo-600 hover:text-indigo-500"
                       >
                         Forgot password?
                       </a>
                     </div>
                   </div>
-                  <div class="mt-2">
+                  <div className="mt-2">
                     <input
                       id="password"
                       value={password}
                       name="password"
                       type="password"
-                      autocomplete="current-password"
+                      autoComplete="current-password"
                       required
-                      class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       onChange={({ target }) => {
                         setPassword(target.value);
                       }}
@@ -142,18 +140,18 @@ function Login() {
                       );
                     }}
                     type="submit"
-                    class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
                     Sign in
                   </button>
                 </div>
               </form>
 
-              <p class="mt-10 text-center text-sm text-gray-500">
+              <p className="mt-10 text-center text-sm text-gray-500">
                 Not a member?
                 <a
                   href="#"
-                  class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                  className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
                 >
                   Join us
                 </a>
