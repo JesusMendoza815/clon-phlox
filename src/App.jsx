e9043c07b944d31bd940618f8a04e5860bce7bb9
Damaris Cubos <damaris.cubos.rosas@gmail.com>
2023-05-18 19:08:41 -0600

    Payment structure added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Payment from "./pages/Payment";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/payment" element={<Payment />} />
       </Routes>
     </>
   );
---
+++ src/pages/Payment.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar'
+import PaymentForm from '../components/PaymentForm'
+
+export default function PaymentGateway() {
+    return (
+        <>
+            <Navbar />
+            <PaymentForm />
+        </>
+    )
+}
+
